f85a0898396fbc16a542de9301018e3243fbfd9f
Josh Taylor <joshrtay@gmail.com>
2016-07-29 23:38:11 -0700

    add css support

--- test/index.js
+++ test/index.js
@@ -11,7 +11,8 @@ const fs = require('fs')
  * Tests
  */
 
-const ELLIOT_URL = "/assets/elliot-2466557555267872952.jpg"
+const ELLIOT_URL = '/assets/elliot-2466557555267872952.jpg'
+const CSS_URL = '/assets/main-15457945457857399425.css'
 
 test('should return url properly', (t) => {
   const url = cloudFS.url(join(__dirname, './elliot.jpg'))
@@ -28,5 +29,12 @@ test('should work with relative path', (t) => {
 test('should write to assets dir', (t) => {
   cloudFS.url('./elliot.jpg')
   t.ok(fs.existsSync(join(process.cwd(), ELLIOT_URL)))
+  t.end()
+})
+
+test('should write css to dir and rewrite url', (t) => {
+  cloudFS.url('./main.css')
+  t.ok(fs.existsSync(join(process.cwd(), CSS_URL)))
+  t.ok(fs.readFileSync(join(process.cwd(), CSS_URL)).indexOf(ELLIOT_URL) > 0)
   t.end()
 })
